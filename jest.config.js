b3c6d41e20a4a673e717922c838cf35ac347307f
ac9189 <welladjusted220@gmail.com>
2018-09-02 11:02:46 +0100

    Moved things

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverageFrom: ['**/src/**/*.js'],
   testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
   moduleNameMapper: {
-    '\\.less$': require.resolve('./__tests__/__mocks__/style-mock')
+    '\\.less$': require.resolve('./client/__tests__/__mocks__/style-mock')
   },
   moduleDirectories: ['node_modules', '<rootDir>/client/src'],
   setupFiles: ['./setupTests.js']
